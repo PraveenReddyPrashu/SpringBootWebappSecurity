b729c0a58269197c2238d420639b4b4ebacc5d7e
Praveen Reddy <praveenreddy291@gmail.com>
2021-05-12 15:27:33 +0530

    web app with springboot security

---
+++ SpringBootWebappSecurity/src/main/java/com/example/WebApp/SpringBootWebappSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebappSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebappSecurityApplication.class, args);
+	}
+
+}
